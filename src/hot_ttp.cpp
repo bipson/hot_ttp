0edef43082f651e77ef3c9ac91a0e84b196ac14a
Philipp Raich <philipp@fedoraphil.lan>
2011-11-16 01:46:03 +0100

    initial commit: as hello-world eclipse project, should be enough to start from

---
+++ src/hot_ttp.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : hot_ttp.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "aloha" << endl; // prints aloha
+	return 0;
+}
